2b7e8a835e73140b53cf89b0d11b7c9c9f66e9b7
grvbrk <gouravbarikofficial@gmail.com>
2024-01-05 21:43:23 +0530

    Restructuring

---
+++ assignments/Week 5/level -1/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import Card from "./components/Card";
+
+function App() {
+  const [count, setCount] = useState({});
+
+  return <>{/* <Card /> */}</>;
+}
+
+export default App;
