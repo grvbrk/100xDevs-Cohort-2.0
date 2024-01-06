5a0ea24d39149a2262a8ff93b5984c6f075aec8f
grvbrk <gouravbarikofficial@gmail.com>
2024-01-06 22:36:34 +0530

    New form structure

--- assignments/Week 5/level -1/src/App.jsx
+++ assignments/Week 5/level -1/src/App.jsx
@@ -1,10 +1,58 @@
+import "./App.css";
 import { useState } from "react";
-import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState({});
+  const [form, setForm] = useState({
+    name: "",
+    desc: "",
+    socials: {},
+    interests: [],
+  });
 
-  return <>{/* <Card /> */}</>;
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  function handleChange(e) {
+    setForm((prevData) => {
+      return { ...prevData, [e.target.name]: e.target.value };
+    });
+  }
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <div className="form-element-text">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          value={form.name}
+          name="name"
+          placeholder="Enter your name"
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-element-text">
+        <label htmlFor="desc">Description</label>
+        <input
+          type="text"
+          value={form.desc}
+          name="desc"
+          placeholder="Describe yourself"
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-element-social">
+        <p>Socials</p>
+        <input type="text" />
+      </div>
+
+      <button type="submit" className="submit-btn">
+        Submit
+      </button>
+    </form>
+  );
 }
 
 export default App;
