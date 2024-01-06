7989943f2ec301419f37d12823b4c699223b842a
grvbrk <gouravbarikofficial@gmail.com>
2024-01-06 23:06:30 +0530

    updated form

--- assignments/Week 5/level -1/src/App.jsx
+++ assignments/Week 5/level -1/src/App.jsx
@@ -5,18 +5,25 @@ function App() {
   const [form, setForm] = useState({
     name: "",
     desc: "",
-    socials: {},
+    socials: [{ platform: "", link: "" }, { platform: "", link: "" }],
     interests: [],
   });
-
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
 
   function handleChange(e) {
     setForm((prevData) => {
       return { ...prevData, [e.target.name]: e.target.value };
     });
+  }
+
+  function handleSocialChange(e, index) {
+    const updatedSocialField = {}
+    console.log(e.target.name, index)
+    
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(form)
   }
 
   return (
@@ -25,6 +32,7 @@ function App() {
         <label htmlFor="name">Name</label>
         <input
           type="text"
+          id="name"
           value={form.name}
           name="name"
           placeholder="Enter your name"
@@ -34,8 +42,9 @@ function App() {
 
       <div className="form-element-text">
         <label htmlFor="desc">Description</label>
-        <input
+        <textarea
           type="text"
+          id="desc"
           value={form.desc}
           name="desc"
           placeholder="Describe yourself"
@@ -44,8 +53,25 @@ function App() {
       </div>
 
       <div className="form-element-social">
-        <p>Socials</p>
-        <input type="text" />
+        <label>Socials</label>
+        {form.socials.map((item, index) => {
+          return (
+            <div key={index} className="form-element-social-links">
+              <input
+                placeholder="Platform"
+                defaultValue={item.platform}
+                name="platform"
+                onChange={(e)=>handleSocialChange(e, index)}
+              />
+              <input
+                placeholder="Link"
+                defaultValue={item.link}
+                name="link"
+                onChange={(e)=>handleSocialChange(e, index)}
+              />
+            </div>
+          );
+        })}
       </div>
 
       <button type="submit" className="submit-btn">
