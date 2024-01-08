465599cbe3a097c38f9dbe0a3c649bf053bec6cf
grvbrk <gouravbarikofficial@gmail.com>
2024-01-08 22:45:19 +0530

    minor styling changes

--- assignments/Week 5/level -1/backend/Models/Card.js
+++ assignments/Week 5/level -1/backend/Models/Card.js
@@ -5,14 +5,11 @@ const mongoose = require("mongoose");
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Must provide name"],
     trim: true,
-    maxlength: [20, "Title can not be more than 20 characters"],
   },
   desc: {
     type: String,
     required: [true],
-    maxlength: [200],
   },
   socials: Array,
   interests: Array,
